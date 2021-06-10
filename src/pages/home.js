fbae454c36b63a386f9b251df89cab5c7a9325ee
Nithya1991 <nithigracy@gmail.com>
2021-06-10 21:55:28 +0530

    updated home js

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,10 +1,20 @@
-import {Link} from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
 export default function Home() {
     return (
-      <div className="home-page">
-        <h1>Landing page</h1>
-        <Link to="/signup">Sign up</Link>
-      </div>
-    )
+        <div className="home-page">
+            <h1>Landing page</h1>
+            <ul className="top-nav">
+                <li className="top-nav-item">
+                    <Link to="/signup">Sign up</Link>
+                </li> 
+                <li className="top-nav-item">
+                    <Link to="/login">Login</Link>
+                </li>
+                <li className="top-nav-item">
+                    <Link to="/products">Product</Link>
+                </li>
+            </ul>
+        </div>
+    );
 }
